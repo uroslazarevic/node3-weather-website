28b236bc4de5b3da546ac8919e41c542a6dc8809
Uros Lazarevic <enlision@gmail.com>
2019-03-27 12:05:44 +0100

    Remove unnecessary concole.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log("Client side javascript file is loaded!");
-
-// fetch("http://localhost:3000/weather?address=!")
-//   .then(res => res.json())
-//   .then(data => {
-//     if (data.error) {
-//       console.log("Error:", data.error);
-//     } else {
-//       console.log(data.location);
-//       console.log(data.forecast);
-//     }
-//   });
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
